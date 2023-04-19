113aa0ecef85502f91e6125192e2212f240d6bed
daqiangli <dqli161@163.com>
2023-04-19 15:28:50 +0800

    change handler

--- pkg/mw/testplan/handler.go
+++ pkg/mw/testplan/handler.go
@@ -4,21 +4,23 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 	testplanmgrpb "github.com/NpoolPlatform/message/npool/smoketest/mgr/v1/testplan"
 	constant "github.com/NpoolPlatform/smoketest-middleware/pkg/const"
+	testplancrud "github.com/NpoolPlatform/smoketest-middleware/pkg/crud/testplan"
 	"github.com/google/uuid"
 )
 
 type Handler struct {
-	ID                *string
-	Name              *string
-	State             *string
-	OwnerID           *string
-	ResponsibleUserID *string
-	Deadline          *uint32
-	Conds             *testplanmgrpb.Conds
-	Offset            *int32
-	Limit             *int32
+	ID        *uuid.UUID
+	Name      *string
+	State     *testplanmgrpb.TestPlanState
+	CreatedBy *uuid.UUID
+	Executor  *uuid.UUID
+	Deadline  *uint32
+	Conds     *testplancrud.Conds
+	Offset    int32
+	Limit     int32
 }
 
 func NewHandler(ctx context.Context, options ...func(context.Context, *Handler) error) (*Handler, error) {
@@ -33,10 +35,11 @@ func NewHandler(ctx context.Context, options ...func(context.Context, *Handler) 
 
 func WithID(id *string) func(context.Context, *Handler) error {
 	return func(ctx context.Context, h *Handler) error {
-		if _, err := uuid.Parse(*id); err != nil {
+		_id, err := uuid.Parse(*id)
+		if err != nil {
 			return err
 		}
-		h.ID = id
+		h.ID = &_id
 		return nil
 	}
 }
@@ -51,12 +54,13 @@ func WithName(name *string) func(context.Context, *Handler) error {
 	}
 }
 
-func WithOwnerID(ownerID *string) func(context.Context, *Handler) error {
+func WithCreatedBy(createdBy *string) func(context.Context, *Handler) error {
 	return func(ctx context.Context, h *Handler) error {
-		if _, err := uuid.Parse(*ownerID); err != nil {
+		_createdBy, err := uuid.Parse(*createdBy)
+		if err != nil {
 			return err
 		}
-		h.OwnerID = ownerID
+		h.CreatedBy = &_createdBy
 		return nil
 	}
 }
@@ -71,47 +75,74 @@ func WithDeadline(deadline *uint32) func(context.Context, *Handler) error {
 	}
 }
 
-func WithResponsibleUserID(userID *string) func(context.Context, *Handler) error {
+func WithState(state *testplanmgrpb.TestPlanState) func(context.Context, *Handler) error {
 	return func(ctx context.Context, h *Handler) error {
-		if userID == nil {
+		if state == nil {
 			return nil
 		}
-		if _, err := uuid.Parse(*userID); err != nil {
-			return err
+		switch *state {
+		case testplanmgrpb.TestPlanState_WaitStart:
+		case testplanmgrpb.TestPlanState_InProgress:
+		case testplanmgrpb.TestPlanState_Finished:
+		case testplanmgrpb.TestPlanState_Overdue:
+		default:
+			return fmt.Errorf("plan state %v invalid", *state)
 		}
-		h.ResponsibleUserID = userID
+		h.State = state
 		return nil
 	}
 }
 
-func WithConds(conds *testplanmgrpb.Conds, offset, limit int32) func(context.Context, *Handler) error {
+func WithExecutor(executor *string) func(context.Context, *Handler) error {
 	return func(ctx context.Context, h *Handler) error {
+		if executor == nil {
+			return nil
+		}
+		_executor, err := uuid.Parse(*executor)
+		if err != nil {
+			return err
+		}
+
+		h.Executor = &_executor
+		return nil
+	}
+}
+
+func WithConds(conds *testplanmgrpb.Conds) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.Conds = &testplancrud.Conds{}
 		if conds == nil {
-			return fmt.Errorf("invalid conds")
+			return nil
 		}
 
 		if conds.ID != nil {
-			if _, err := uuid.Parse(conds.GetID().GetValue()); err != nil {
+			id, err := uuid.Parse(conds.GetID().GetValue())
+			if err != nil {
 				return err
 			}
-		}
-		if conds.ResponsibleUserID != nil {
-			if _, err := uuid.Parse(conds.GetResponsibleUserID().GetValue()); err != nil {
-				return err
-			}
+			h.Conds.ID = &cruder.Cond{Op: h.Conds.ID.Op, Val: id}
 		}
 
-		h.Conds = conds
-
-		if h.Offset == nil {
-			offset = constant.DefaultRowLimit
+		if conds.State != nil {
+			h.Conds.State = &cruder.Cond{Op: h.Conds.State.Op, Val: conds.State}
 		}
-		h.Offset = &offset
+		return nil
+	}
+}
+
+func WithOffset(offset int32) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.Offset = offset
+		return nil
+	}
+}
+
+func WithLimit(limit int32) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
 		if limit == 0 {
 			limit = constant.DefaultRowLimit
 		}
-		h.Limit = &limit
-
+		h.Limit = limit
 		return nil
 	}
 }
