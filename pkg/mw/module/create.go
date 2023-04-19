68cfa40b0c909e4d64c4375a7eff57b4febd014d
daqiangli <dqli161@163.com>
2023-04-19 10:39:01 +0800

    update module create.go

--- pkg/mw/module/create.go
+++ pkg/mw/module/create.go
@@ -4,11 +4,10 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/NpoolPlatform/message/npool/smoketest/mgr/v1/module"
-	npool "github.com/NpoolPlatform/message/npool/smoketest/mw/v1/module"
+	npool "github.com/NpoolPlatform/message/npool/smoketest/mgr/v1/module"
+	modulecrud "github.com/NpoolPlatform/smoketest-middleware/pkg/crud/module"
 	"github.com/NpoolPlatform/smoketest-middleware/pkg/db"
 	"github.com/NpoolPlatform/smoketest-middleware/pkg/db/ent"
-	modulecrud "github.com/NpoolPlatform/smoketest-middleware/pkg/crud/module"
 )
 
 type createHandler struct {
@@ -19,23 +18,10 @@ func (h *createHandler) validate() error {
 	if h.Name == nil {
 		return fmt.Errorf("invalid name")
 	}
-	return nil
-}
-
-func (h *createHandler) createModule(ctx context.Context, tx *ent.Tx) error {
-	info, err := modulecrud.CreateSet(
-		tx.Module.Create(),
-		&module.ModuleReq{
-			Name:        h.Name,
-			Description: h.Description,
-		},
-	).Save(ctx)
-	if err != nil {
-		return err
+	const leastNameLen = 4
+	if len(*h.Name) < leastNameLen {
+		return fmt.Errorf("name %v too short", *h.Name)
 	}
-
-	id := info.ID.String()
-	h.ID = &id
 	return nil
 }
 
@@ -48,8 +34,18 @@ func (h *Handler) CreateModule(ctx context.Context) (info *npool.Module, err err
 		return nil, err
 	}
 
-	err = db.WithTx(ctx, func(_ctx context.Context, tx *ent.Tx) error {
-		if err := handler.createModule(_ctx, tx); err != nil {
+	if _, err = h.ExistModuleConds(ctx); err != nil {
+		return nil, fmt.Errorf("name already exist")
+	}
+
+	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		if _, err := modulecrud.CreateSet(
+			cli.Module.Create(),
+			&modulecrud.Req{
+				Name:        h.Name,
+				Description: h.Description,
+			},
+		).Save(ctx); err != nil {
 			return err
 		}
 		return nil
@@ -57,6 +53,5 @@ func (h *Handler) CreateModule(ctx context.Context) (info *npool.Module, err err
 	if err != nil {
 		return nil, err
 	}
-
 	return h.GetModule(ctx)
 }
