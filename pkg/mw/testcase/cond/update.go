482019bedebecdf8767457dc40e2deebbe8263a2
daqiangli <dqli161@163.com>
2023-04-23 11:03:14 +0800

    add update

---
+++ pkg/mw/testcase/cond/update.go
@@ -0,0 +1,39 @@
+package cond
+
+import (
+	"context"
+
+	npool "github.com/NpoolPlatform/message/npool/smoketest/mgr/v1/testcase/cond"
+	crud "github.com/NpoolPlatform/smoketest-middleware/pkg/crud/testcase/cond"
+	"github.com/NpoolPlatform/smoketest-middleware/pkg/db"
+	"github.com/NpoolPlatform/smoketest-middleware/pkg/db/ent"
+)
+
+func (h *Handler) UpdateCond(ctx context.Context) (info *npool.Cond, err error) {
+	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		if _, err := crud.UpdateSet(
+			cli.Cond.UpdateOneID(*h.ID),
+			&crud.Req{
+				ID:             h.ID,
+				TestCaseID:     h.TestCaseID,
+				CondTestCaseID: h.CondTestCaseID,
+				Index:          h.Index,
+				ArgumentMap:    h.ArgumentMap,
+				CondType:       h.CondType,
+			},
+		).Save(_ctx); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	info, err = h.GetCond(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return info, nil
+}
